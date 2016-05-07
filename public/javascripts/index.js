916242f51380ca80608fa2a52e4a987f1150efc0
SkyZH <iSkyZH@gmail.com>
2016-05-07 16:43:30 +0800

    fix string format

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,16 +7,16 @@ function filterData(type, field) {
         .map(field);
 }
 
-String.format = function() {
-    if(arguments.length === 0)
-        return null;
-
-    var str = arguments[0];
-    for(var i=1;i<arguments.length;i++) {
-        var re = new RegExp('\\{' + (i-1) + '\\}','gm');
-        str = str.replace(re, arguments[i]);
-    }
-    return str;
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
 }
 
 (function() {
